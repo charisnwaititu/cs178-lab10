af028aa48de88ffb29ecd478109a9af777758884
charisnwaititu <charis.waititu@drake.edu>
2026-03-05 12:55:50 -0600

    Update rating

--- MoviesInterface.py
+++ MoviesInterface.py
@@ -44,12 +44,13 @@ def print_all_movies():
 
 
 def update_rating():
-    """
-    Prompt user for a Movie Title.
-    Prompt user for a rating (integer).
-    Append the rating to the movie's Ratings list in the database.
-    """
-    print("updating rating")
+    title = input("What is the movie title? ")
+    rating = int(input("What is the rating (integer): "))
+    table.update_item(
+        Key={"Title": title},
+        UpdateExpression="SET Ratings = list_append(Ratings, :r)",
+        ExpressionAttributeValues={':r': [rating]}
+    )
 
 def delete_movie():
     """
